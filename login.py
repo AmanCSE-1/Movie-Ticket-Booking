2714f92a16d4441cc53a16033aca6531e0e1c7a1
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-04-17 13:11:59 +0530

    Create login.py
    This program checks the user credentials and fetches result from the SQLite Database. If correct credentials are entered by the user, then he is redirected to Movie Interface where he can select any movie and book ticktets.

---
+++ login.py
@@ -0,0 +1,71 @@
+def logIn():
+    logIn = Tk()
+    logIn.geometry("1024x786")
+    logIn.resizable(0,0)
+    logIn.title("MovieZone - Movie Ticket Booking")
+    #logIn.iconbitmap('path')
+    global username
+    
+    bgColor1 = "#C7EFCF"; bgColor3= "#FE5F55"
+    fgColor1 = "#EEF5DB"
+    banner = LabelFrame(logIn, width=1024, height=785, bg=bgColor1, padx=70, pady=20)
+    banner.place(x=0, y=0)
+    
+    box1 = LabelFrame(logIn, width=400, height=80, bg=bgColor3)
+    box1.place(x=300, y=150)
+    
+    label1 = Label(box1, text="LOG IN", font=('Cambria', 26, 'bold'), fg=fgColor1, bg=bgColor3)
+    label1.place(x=150, y=20)
+    
+    box2 = LabelFrame(logIn, width=400, height=400, bg=fgColor1)
+    box2.place(x=300, y=230)
+    
+    Label(box2, text="Username", font=('comic', 12), fg=bgColor3).place(x=30, y=30)
+    entry1 = Entry(box2, width=40, bd=3)
+    entry1.place(x=32, y=63)
+    
+    Label(box2, text="Password", font=('comic', 12), fg=bgColor3).place(x=30, y=120)
+    entry2 = Entry(box2, width=40, bd=3)
+    entry2.place(x=32, y=153)
+    
+    
+    
+    #---------------------------------------------------------------#
+    #-----------------Validate the user details---------------------#
+    
+    def validate(entry1, entry2):
+        global username
+        global email
+        
+        username = entry1.get()
+        password = entry2.get()
+
+        if username=="" and password=="":
+            messagebox.showerror("Movie-Zone", "Please enter Username and Password")
+            
+        else:
+            try:
+                connection = sqlite3.connect('ESD/Homepage/movieTicket.db')
+                cur = connection.cursor()
+
+                cur.execute('SELECT username, email FROM user WHERE username=? AND password=?', (username, password))
+                row = cur.fetchone()
+                email = row[1]
+                if row is None:
+                    tkinter.messagebox.showerror("Movie-Zone", "Incorrect Credentials")
+                else:
+                    createNew(logIn, movie)
+
+                cur.close()
+                connection.commit()
+                connection.close()
+            except:
+                tkinter.messagebox.showerror("Movie-Zone", "Incorrect Credentials")
+        #--------------End of Validate--------------------------------#
+            
+            
+    LoginB = Button(box2, text="Log In", width=15, bg=bgColor3, fg=fgColor1, font=('cambria', 14), 
+                    command=lambda: validate(entry1, entry2))
+    LoginB.place(x=120, y=230)
+    
+    logIn.mainloop()
