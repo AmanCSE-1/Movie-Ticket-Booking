0389f09ebc8adba51a972b2c4ef2d3a54f0e2b50
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-11-14 23:27:12 +0530

    Fixes
    Fixed the links to other modules and call to their functions.
    Path of the image is changed to assets folder

--- home.py
+++ home.py
@@ -1,6 +1,17 @@
+## Importing the Libraries
+## Tkinter -> Standard GUI Library in Python provides fast and easy way to create GUI applications. 
+## PIL -> Supports opening anf manipulating different image file formats .
 from tkinter import *
-import tkinter.messagebox
-from PIL import ImageTk, Image
+import PIL.ImageTk
+import PIL.Image
+
+
+## Importing the other modules
+import signup
+import login
+import movie
+
+
 
 def createNew(base, new):
     base.destroy()
@@ -27,7 +38,7 @@ def home():
     button1 = Button(navbar, text="Home", padx=5, bg=bgColor)
     button1.grid(row=0, column=2)
 
-    button2 = Button(navbar, text="Movie", padx=5, bg=bgColor, command= lambda: createNew(home, movie))
+    button2 = Button(navbar, text="Movie", padx=5, bg=bgColor, command= lambda: createNew(home, movie.movie))
     button2.grid(row=0, column=3)
 
     button3 = Button(navbar, text="My Bookings", padx=5, bg=bgColor)
@@ -54,19 +65,19 @@ def home():
     tagline.place(x=80, y=130)
     
     signUpB = Button(banner, text="Sign Up for Free", bg="#F1D9A7", fg="#000066", height=2, padx=5, font=('Cambria'), 
-                     command= lambda: createNew(home, signUp))
+                     command= lambda: createNew(home, signup.signUp))
     signUpB.place(x=70, y=250)
     
     label = Label(banner, text="OR", font=('Arial', 14, 'bold'), bg=bgColor1, pady=10, fg="#DEE2FF")
     label.place(x=250, y=252)
     
     logInB = Button(banner, text="Log In", bg="#F1D9A7", fg="#000066", height=2, padx=10, font=('Cambria'), 
-                   command= lambda: createNew(home, logIn))
+                   command= lambda: createNew(home, login.logIn))
     logInB.place(x=340, y=250)
     
-    image = Image.open('ESD/Homepage/home.png')
-    image = image.resize((320, 270), Image.ANTIALIAS)
-    test = ImageTk.PhotoImage(image)
+    image = PIL.Image.open('assets/home.png')
+    image = image.resize((320, 270), PIL.Image.ANTIALIAS)
+    test = PIL.ImageTk.PhotoImage(image)
     main = Label(banner, height=270, width=320, padx=5, pady=5, image=test, bg="#DEE2FF")
     main.image = test
     main.place(x=550, y=50)
@@ -80,9 +91,9 @@ def home():
     banner2.place(x=0, y=440)
     
     def placemovie2(path, w, h, xcoordinate, ycoordinate, name):
-        image = Image.open(path)
-        image = image.resize((w-5, h-5), Image.ANTIALIAS)
-        test = ImageTk.PhotoImage(image)
+        image = PIL.Image.open(path)
+        image = PIL.image.resize((w-5, h-5), PIL.Image.ANTIALIAS)
+        test = PIL.ImageTk.PhotoImage(image)
         main = Label(banner2, height=h, width=w, padx=5, pady=5, image=test, bg="#DEE2FF")
         main.image = test
         main.place(x=xcoordinate, y=ycoordinate)
@@ -92,9 +103,9 @@ def home():
     section2 = Label(banner2, text="Exclusive Offers on Blockbuster Hit", bg=bgColor2, fg=fgColor2, font=('Cambria',14, 'italic'))
     section2.place(x=40, y=20)
 
-    placemovie2("ESD/Homepage/sooryavanshi.jpg", 172, 140, 40, 70, "Sooryavanshi")
-    placemovie2("ESD/Homepage/black-widow.jpg", 172, 140, 340, 70, "Black Widow")
-    placemovie2("ESD/Homepage/saina.jpg", 172, 140, 640, 70, "Sania")
+    placemovie2("assets/images/sooryavanshi.jpg", 172, 140, 40, 70, "Sooryavanshi")
+    placemovie2("assets/images/black-widow.jpg", 172, 140, 340, 70, "Black Widow")
+    placemovie2("assets/images/saina.jpg", 172, 140, 640, 70, "Sania")
     
     home.mainloop()
     
